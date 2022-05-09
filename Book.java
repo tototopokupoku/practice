7c137c676b262a7d39ab2d5b812b33f72a708e05
tototopokupoku <ito.hitoshi18@gmail.com>
2022-05-09 19:20:02 +0900

    Book.java has been fixed now

--- Book.java
+++ Book.java
@@ -1,4 +1,4 @@
-public class Book extends TngibleAsset{
+public class Book extends TangibleAsset{
 	private String isbn;
 	public Book(String name, int price, String color, String isbn){
 		super(name, price, color);
